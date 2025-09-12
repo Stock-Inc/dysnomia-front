80b62b8b2a6df45eb55ffcce765f1a7e00708fbd
lZh1r <electroxhackerpig09@gmail.com>
2025-09-12 10:16:18 +0300

    transferred font and color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+const Dustismo = localFont({
+    src: "./Dustismo.ttf",
+    variable: "--font-dustismo"});
 
 export const metadata: Metadata = {
     title: "Dysnomia",
@@ -25,7 +19,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${Dustismo.variable} antialiased`}
             >
             {children}
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-
+            <h1 className="font-main text-2xl"></h1>
         </div>
     );
 }
