80b7d2c62ccaf9d0b78ad74ab6f20f00578817e7
lZh1r <electroxhackerpig09@gmail.com>
2025-09-13 13:26:30 +0300

    made button respond to being disabled

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,19 +3,22 @@ import React from "react";
 
 export default function Button(
     {
-        children, onClickAction, variant = "accent", className, type
+        children, onClickAction, variant = "accent", className, type, disabled,
     }: {
         children: React.ReactNode, onClickAction?: () => void,
         variant?: "accent" | "outline", className?: string,
-        type?: "submit" | "reset" | "button"
+        type?: "submit" | "reset" | "button", disabled?: boolean
     }) {
     return (
         <button
+            disabled={disabled}
             type={type}
-            className={`${variant === "accent" ? "bg-accent text-white" :
-                "border-2 border-accent bg-light-background text-accent hover:text-shadow-glow"} 
+            className={`${variant === "accent" ? "bg-accent text-white disabled:bg-dark-accent disabled:text-foreground" :
+                "border-2 disabled:border-dark-accent border-accent bg-light-background text-accent hover:text-shadow-glow" +
+                "disabled:text-dark-accent disabled:hover:text-shadow-none"} 
                 rounded-2xl font-main text-center text-2xl cursor-pointer p-3
                 hover:drop-shadow-accent hover:scale-105 transition-all max-md:text-xl
+                disabled:cursor-default disabled:hover:drop-shadow-none disabled:hover:scale-100
             ${className}`}
             //TODO: make it prettier
             aria-label={typeof children === "object" && "props" in children! ?
