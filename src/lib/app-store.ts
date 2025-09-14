aac6a4c1c63e8fd8d6a20731bf28d5d2d39ad7b4
lZh1r <electroxhackerpig09@gmail.com>
2025-09-14 21:49:43 +0300

    display name added to zustand store

--- src/lib/app-store.ts
+++ src/lib/app-store.ts
@@ -4,12 +4,16 @@ import {persist} from "zustand/middleware";
 type AppStoreState = {
     username: string,
     setUsername: (username: string) => void,
+    displayName: string,
+    setDisplayName: (displayName: string) => void,
 }
 
 export const appStore = create(persist<AppStoreState>(
     (set) => ({
         username: '',
-        setUsername: (username: string) => set({username: username}),
+        setUsername: username => set({username: username}),
+        displayName: '',
+        setDisplayName: displayName => set({displayName: displayName}),
     }), {
         name: "dysnomia-client-store"
     })
