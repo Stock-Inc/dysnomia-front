536382c2dca35d82660e06408043e6d08682d8b3
lZh1r <electroxhackerpig09@gmail.com>
2025-09-15 22:23:33 +0300

    pressing escape kicks you out of your current active chat

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,24 @@
 "use client";
+import ChatArea from "@/components/home/ChatArea";
+import {appStore} from "@/lib/app-store";
+import {useEffect} from "react";
 
 export default function Page() {
-    return (
-        <div className="">
 
+    const store = appStore();
+
+    useEffect(() => {
+        function handleEsc(e: KeyboardEvent) {
+            if (e.key === "Escape" && store.isSidebarOpen) store.setCurrentChatId("");
+        }
+        window.addEventListener("keydown", handleEsc);
+        return () => window.removeEventListener("keydown", handleEsc);
+    }, [store.isSidebarOpen]);
+
+    return (
+        <div className={`h-screen transition-all max-sm:pt-21 sm:pr-21 sm:pl-21 font-main flex flex-col justify-center`}>
+            <ChatArea />
         </div>
+
     );
 }
