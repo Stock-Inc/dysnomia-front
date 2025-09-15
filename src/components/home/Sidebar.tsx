47ff58245a3569cfeef4e9671e32e93a1f74d936
lZh1r <electroxhackerpig09@gmail.com>
2025-09-15 10:03:28 +0300

    bar now retains its state

--- src/components/home/Sidebar.tsx
+++ src/components/home/Sidebar.tsx
@@ -1,16 +1,18 @@
 "use client";
 import Button from "@/components/Button";
 import {SidebarIcon} from "lucide-react";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
+import {appStore} from "@/lib/app-store";
 
 const initialWidth = 240;
 
 export default function Sidebar() {
-    const [isOpen, setIsOpen] = useState(true);
+    const store = appStore();
     const isResizing = useRef(false);
     const [width, setWidth] = useState(initialWidth);
     const sidebarRef = useRef(null);
 
+    //TODO: disable the awkward slide that the sidebar does if it is initially closed
     //TODO: Make sidebar resizable
 
     // useEffect(() => {
@@ -38,17 +40,19 @@ export default function Sidebar() {
                 variant={"outline"}
                 className={`z-30 absolute m-4 hover:drop-shadow-none border-foreground 
                 text-foreground hover:scale-none hover:border-accent hover:text-accent`}
-                onClickAction={() => setIsOpen(o => !o)}
+                onClickAction={() => {
+                    store.setIsSidebarOpen();
+                }}
             >
                 <SidebarIcon/>
             </Button>
             <div
                 ref={sidebarRef}
                 className={
-                    `max-sm:w-full transition-transform ease-in-out duration-300 w-90
-                    ${!isOpen && "-translate-x-full border-r-2 border-card-border"} 
+                    `max-sm:w-full transition-transform w-90 left-0 border-r-1 border-card-border
+                    ${store.isSidebarOpen ? "translate-x-0" : "-translate-x-full"} 
                     h-screen max-h-screen bg-light-background absolute
-                    z-10 left-0 top-0 flex flex-col font-main`
+                    z-10 top-0 flex flex-col font-main`
                 }
             >
                 <div onMouseDown={(e) => {
@@ -56,7 +60,7 @@ export default function Sidebar() {
                         isResizing.current = true;
                     }}
                      // style={{left: width}}
-                     className={`${!isOpen && "hidden"} max-sm:hidden fixed cursor-ew-resize w-0.5 h-full bg-card-border left-90`}></div>
+                     className={`${!store.isSidebarOpen && "hidden"} max-sm:hidden fixed cursor-ew-resize w-0.5 h-full bg-card-border left-90`}></div>
                 <h2 className={"text-center text-3xl border-b-2 border-card-border p-6"}>Chats</h2>
             </div>
         </div>
--- src/lib/app-store.ts
+++ src/lib/app-store.ts
@@ -6,14 +6,18 @@ type AppStoreState = {
     setUsername: (username: string) => void,
     displayName: string,
     setDisplayName: (displayName: string) => void,
+    isSidebarOpen: boolean,
+    setIsSidebarOpen: () => void,
 }
 
 export const appStore = create(persist<AppStoreState>(
-    (set) => ({
+    (set, get) => ({
         username: '',
         setUsername: username => set({username: username}),
         displayName: '',
         setDisplayName: displayName => set({displayName: displayName}),
+        isSidebarOpen: true,
+        setIsSidebarOpen: () => set({isSidebarOpen: !get().isSidebarOpen})
     }), {
         name: "dysnomia-client-store"
     })
