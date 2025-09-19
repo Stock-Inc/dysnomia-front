16f34f4e569a93d1c29ad1ac818f5508f4c36a7a
lZh1r <electroxhackerpig09@gmail.com>
2025-09-19 17:58:27 +0300

    chat area input field dynamic resizing (ctrl v breaks it, but idc)

--- src/components/home/ChatArea.tsx
+++ src/components/home/ChatArea.tsx
@@ -1,6 +1,6 @@
 import {appStore} from "@/lib/app-store";
 import SockJS from "sockjs-client";
-import {useEffect, useRef, useState} from "react";
+import React, {ChangeEvent, useEffect, useRef, useState} from "react";
 import {Client} from "@stomp/stompjs";
 import MessageBox from "@/components/home/MessageBox";
 import {SendHorizonal} from "lucide-react";
@@ -16,7 +16,7 @@ export interface ChatMessage {
 export default function ChatArea() {
     const store = appStore();
     const stompClient = useRef<Client | null>(null);
-
+    const textareaRef = useRef<null | HTMLTextAreaElement>(null);
     const [messages, setMessages] = useState<undefined|ChatMessage[]>(undefined);
     const [input, setInput] = useState("");
 
@@ -44,8 +44,6 @@ export default function ChatArea() {
                     });
 
                 stompClient.current!.publish({destination: "/app/history", body: JSON.stringify([]), headers: {'content-type': 'application/json'}});
-
-
             },
 
             onStompError: (frame) => {
@@ -55,16 +53,29 @@ export default function ChatArea() {
 
         stompClient.current.activate();
 
-        return () => {
-            stompClient.current?.deactivate();
-        };
-
     }, []);
+
+    function handleKeyPress(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+        if (e.key === "Enter" && e.shiftKey) return;
+        if (e.key === "Enter" && input.trim() !== "") {
+            e.preventDefault();
+            setInput("");
+            textareaRef.current!.rows = 1;
+        }
+    }
+
+    function handleInputChange(e: ChangeEvent<HTMLTextAreaElement>) {
+        setInput(e.currentTarget.value);
+        if (textareaRef.current!.value.split("\n").length <= 5) {
+            textareaRef.current!.rows = 1 + textareaRef.current!.value.split("\n").length;
+        }
+    }
 
     return (
         store.currentChatId ?
             <div className={`bg-chat-background max-sm:border-t-2 sm:border-x-2 border-card-border
              h-screen space-y-2 max-h-screen overflow-y-scroll [&::-webkit-scrollbar-track]:border-card-border
+             [&::-webkit-scrollbar-thumb]:hover:bg-accent [&::-webkit-scrollbar-thumb]:transition-all
              flex flex-col [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-light-background
              [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-card-border [&::-webkit-scrollbar-track]:border-l-2
              ${store.isSidebarOpen && "max-md:hidden"}`}>
@@ -72,15 +83,23 @@ export default function ChatArea() {
                     messages?.map((message) => <MessageBox key={message.id} isOuter={store.username !== message.name} message={message}/>)
                 }
                 <div className={`sticky bottom-0 w-full left-0 h-fit flex group ${!messages && "hidden"}`}>
-                    {/*TODO: resizing*/}
-                    <textarea value={input} placeholder={"Write a message..."}
-                              onChange={(e) => setInput(e.currentTarget.value)}
+                    <textarea value={input}
+                              placeholder={"Write a message..."}
+                              rows={1}
+                              ref={textareaRef}
+                              maxLength={1024}
+                              onKeyDown={handleKeyPress}
+                              onChange={handleInputChange}
                               className={`bg-light-background resize-none w-full min-h-15 p-2 text-lg group-has-focus:border-accent
+                              [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-background
+                              [&::-webkit-scrollbar-thumb]:hover:bg-accent [&::-webkit-scrollbar-thumb]:transition-all
+                              [&::-webkit-scrollbar-thumb]:cursor-default
+                              [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-card-border
                               border-t-2 border-card-border focus:outline-none focus:border-accent transition-all h-auto`}>
                     </textarea>
                     <button spellCheck={"false"} className={`
                         place-self-center p-5 rounded-none bg-light-background border-t-2 border-card-border cursor-pointer
-                        group-has-focus:border-accent transition-all
+                        group-has-focus:border-accent transition-all h-full
                         ${!input.trim() && "text-light-background pointer-events-none focus:bg-light-background"}
                         hover:text-accent hover:bg-card-border flex justify-center focus:outline-none focus:bg-card-border`}>
                         <SendHorizonal className={"place-self-center w-8 h-8"}/>
