6cd0b8e990ca6e23d6d6c602f8379dd6e400f1bd
lZh1r <electroxhackerpig09@gmail.com>
2025-09-23 10:16:10 +0300

    installed motion.dev, added animations to te landing page, and created SplitText component to replicate 300 euro splitText feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,68 @@
 import Image from "next/image";
 import Button from "@/components/Button";
 import Link from "next/link";
+import {motion, useAnimate} from "motion/react";
+import {stagger} from "motion/react";
+import {useEffect} from "react";
+import SplitText from "@/components/SplitText";
 
 export default function Page() {
+
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animate(".buttons", {opacity: 1, y: 0}, {duration: 0.5, ease: "backOut", delay: stagger(0.1)});
+        const sloganElements = document.querySelectorAll(".slogan");
+        animate(sloganElements, {opacity: 1, y: 0}, {duration: 0.3, delay: stagger(0.1)});
+        const titleElements = document.querySelectorAll(".title");
+        animate(titleElements, {opacity: 1, y: 0}, {duration: 0.3, delay: stagger(0.1)});
+    }, [animate]);
+
     return (
         <div className="flex flex-col items-center justify-center p-6 font-main space-y-5">
-            <Image
-                fetchPriority={"high"}
-                priority={true}
-                aria-label={"Dysnomia logo"}
-                className={"drop-shadow-accent max-sm:w-1/2 mb-10 sm:mb-20"}
-                src={"./icon.svg"}
-                alt={"logo"}
-                width={300}
-                height={300} />
-            <h1 className="md:text-5xl text-4xl">Dysnomia</h1>
-            <p className="md:text-2xl text-xl text-center">Free open source messenger that values your
-                <span className={"text-accent font-bold text-shadow-glow"}> privacy</span>.</p>
-            <div className="flex justify-evenly sm:space-x-10 max-sm:flex-col max-sm:space-y-4">
-                <Link className="h-fit" href={"/login"}><Button onClickAction={() => {}}>Get Started</Button></Link>
-                <Button variant={"outline"} onClickAction={() => {}}>About Us</Button>
+            <motion.div
+                initial={{opacity: 0, y: -20}}
+                animate={{opacity: 1, y: 0}}
+                transition={{duration: 0.5, ease: "backOut", delay: 0.75}}
+                className={"flex mb-10 sm:mb-20 justify-center"}
+            >
+                <Image
+                    fetchPriority={"high"}
+                    priority={true}
+                    aria-label={"Dysnomia logo"}
+                    className={"drop-shadow-accent max-sm:w-1/2"}
+                    src={"./icon.svg"}
+                    alt={"logo"}
+                    width={300}
+                    height={300} />
+            </motion.div>
+            <SplitText
+                ref={scope}
+                split={"symbol"}
+                initial={{opacity: 0}}
+                childrenClassName={"title"}
+                className={"md:text-5xl text-4xl"}>
+                Dysnomia
+            </SplitText>
+            <SplitText initial={{y: 10, opacity: 0}} ref={scope} childrenClassName={"slogan"}
+                       className="md:text-2xl text-xl text-center">Free open source messenger that values your
+                <span className={"text-accent font-bold text-shadow-glow"}> privacy</span>.</SplitText>
+            <div
+                ref={scope}
+                className="flex justify-evenly sm:space-x-10 max-sm:flex-col max-sm:space-y-4"
+                >
+                <motion.div
+                    className={"buttons h-fit w-fit place-self-center"}
+                    initial={{opacity: 0, y: 50}}
+                >
+                    <Link className={"h-fit place-self-center"} href={"/login"}><Button onClickAction={() => {}}>Get Started</Button></Link>
+                </motion.div>
+                <motion.div
+                    className={"buttons h-fit w-fit place-self-center"}
+                    initial={{opacity: 0, y: 50}}
+                >
+                    <Button variant={"outline"} onClickAction={() => {}}>About Us</Button>
+                </motion.div>
             </div>
         </div>
     );
---
+++ src/components/SplitText.tsx
@@ -0,0 +1,101 @@
+import {motion, VariantLabels} from "motion/react";
+import {TargetAndTransition} from "motion";
+import React, {ReactElement, useMemo} from "react";
+
+export default function SplitText(
+    {
+        children,
+        ref,
+        initial,
+        childrenClassName,
+        className,
+        split,
+    }: {
+        children: React.ReactNode;
+        ref: React.RefObject<HTMLDivElement>;
+        initial: boolean | undefined | TargetAndTransition | VariantLabels;
+        childrenClassName: string;
+        className?: string;
+        split?: "word" | "symbol";
+    }) {
+
+    const toRender = useMemo(() => {
+        const result: React.ReactNode[] = [];
+        React.Children.map(children, child => {
+            switch (typeof child) {
+                case "bigint":
+                case "boolean":
+                case "number":
+                case "symbol": {
+                    result.push(
+                        <motion.span
+                            key={child + childrenClassName + child}
+                            className={`${childrenClassName}`}
+                            initial={initial}
+                        >
+                            {child + " "}
+                        </motion.span>
+                    );
+                } break;
+                case "string": {
+                    if (!split || split === "word") {
+                        child.split(" ").forEach((word, i) => {
+                            result.push(
+                                <motion.span
+                                    key={i + word + childrenClassName}
+                                    className={`${childrenClassName}`}
+                                    initial={initial}
+                                >
+                                    {word + " "}
+                                </motion.span>
+                            );
+                        });
+                    } else {
+                        child.split(" ").forEach((word, i) => {
+                            word.split("").forEach((letter, i) => {
+                                result.push(
+                                    <motion.span
+                                        key={i + word + childrenClassName + letter}
+                                        className={`${childrenClassName}`}
+                                        initial={initial}
+                                    >
+                                        {letter}
+                                    </motion.span>
+                                );
+                            });
+                            result.push(
+                                <span key={i + word + childrenClassName}>
+                                    {" "}
+                                </span>
+                            );
+                        });
+                    }
+                } break;
+                default: {
+                    if (!child) return;
+                    if (React.isValidElement(child)) {
+                        const props = child.props as React.HTMLAttributes<HTMLElement>;
+                        result.push(
+                            <motion.span
+                                key={(child.key ?? "child") + childrenClassName}
+                                className={`${childrenClassName} ${props.className}`}
+                                initial={initial}
+                            >
+                                {props.children}
+                            </motion.span>
+                        );
+                    }
+                }
+            }
+        });
+        return result;
+    }, [children]);
+
+    return (
+        <div ref={ref} className={className}>
+            {
+                toRender
+            }
+        </div>
+    );
+}
