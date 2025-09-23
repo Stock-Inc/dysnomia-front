4efe85a17c36cdd7f1175866664de9005467f91c
lZh1r <electroxhackerpig09@gmail.com>
2025-09-23 17:26:58 +0300

    extracted reply fetching logic into its own one-off hook and made some adjustments to MessageBox component in general

--- src/components/home/chat/MessageBox.tsx
+++ src/components/home/chat/MessageBox.tsx
@@ -1,60 +1,61 @@
 import {ChatMessage} from "@/components/home/chat/ChatArea";
-import { motion } from "motion/react";
-import {useEffect, useState} from "react";
-
-const placeholderMessage: ChatMessage = {
-    id: 0,
-    name: "...",
-    message: "...",
-    date: 0,
-    reply_id: undefined,
-};
+import {motion} from "motion/react";
+import useReplyMessage from "@/hook/useReplyMessage";
 
 export default function MessageBox(
     {message, isOuter, doubleClickHandler}:
     {message: ChatMessage, isOuter: boolean, doubleClickHandler:() => void}
 ) {
-    const [replyMessage, setReplyMessage] = useState<null | ChatMessage>(null);
+    const replyMessage = useReplyMessage(message.reply_id);
 
-    useEffect(() => {
-        if (message.reply_id === 0) return;
-        fetch(`https://api.femboymatrix.su/message/${message.reply_id}`, {method: "GET"})
-            .then(r => r.json()).catch(e => console.log(e))
-            .then(msg => {
-                setReplyMessage(msg);
-            }).catch(e => console.log(e));
-    }, [message.reply_id]);
+    const replyPreview = () => {
+        const loading = replyMessage.loading;
+        const profileName = loading ? "..." : replyMessage.data?.name;
+        const text = loading ? "..." : (replyMessage.error || replyMessage.data?.message);
+        return (
+            <>
+                <p className={"text-sm"}>{profileName}</p>
+                <p>{text}</p>
+            </>
+        );
+    };
 
     //TODO: clicking on a reply text scrolls to the original message
 
     return (
         <motion.div
             onDoubleClick={doubleClickHandler}
-            className={`text-lg rounded-2xl p-1 flex flex-col max-w-300 w-fit ${isOuter ? 
+            className={`text-lg rounded-2xl p-1 flex flex-col max-w-200 w-fit ${isOuter ? 
             "place-self-start rounded-bl-none bg-card-border" :
             "place-self-end rounded-br-none text-white bg-accent"}`}
             initial={{opacity: 0}}
             animate={{opacity: 1}}
+            transition={{duration: 0.5}}
         >
             <p className={"text-md p-1 mb-1 text-shadow-white-glow"}>
                 {message.name ? message.name : "anon"}
             </p>
             {message.reply_id !== 0 &&
-                <div className={`p-1 mx-1 line-clamp-2 overflow-hidden text-md ${isOuter ?
-                    "bg-chat-outer-reply-background border-l-4 border-foreground rounded-r-xl" : 
-                    "bg-dark-accent border-r-4 border-white rounded-l-xl"}`}>
-                    <p className={"text-sm"}>{(replyMessage ?? placeholderMessage).name}</p>
-                    {(replyMessage ?? placeholderMessage).message}
+                <div onClick={() => {
+                    if (!replyMessage.error && !replyMessage.loading) {
+                        //Scroll stuff
+                    }
+                }} className={`p-1 mx-1 line-clamp-2 overflow-hidden text-md cursor-pointer ${isOuter ?
+                    "bg-chat-outer-reply-background border-l-4 border-foreground rounded-r-xl hover:bg-chat-outer-reply-background-focus" : 
+                    "bg-dark-accent border-r-4 border-white rounded-l-xl hover:bg-dark-accent-focus"}`}>
+                    {replyPreview()}
                 </div>
             }
             <div className={"flex justify-between"}>
                 <p className={"p-1 wrap-anywhere"}>{message.message}</p>
                 <p className={`text-sm place-self-end p-1 select-none pointer-events-none ${isOuter ? "text-muted-foreground" : "text-gray-100"}`}>
-                    {new Date(message.date).toLocaleTimeString("en-US", {
-                        hour12: false ,
-                        localeMatcher: "best fit",
-                        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, //TODO: fix time
-                    })}
+                    {
+                        new Date(message.date).toLocaleTimeString("en-US", {
+                            hour12: false ,
+                            localeMatcher: "best fit",
+                            timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, //TODO: fix time
+                        })
+                    }
                 </p>
             </div>
         </motion.div>
