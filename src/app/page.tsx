e507c11deb79c80d36eb8339616e9b445d3c2d9c
lZh1r <electroxhackerpig09@gmail.com>
2025-09-25 17:50:20 +0300

    centered everything on the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,50 +20,52 @@ export default function Page() {
     }, [animate]);
 
     return (
-        <div className="flex flex-col items-center justify-center p-6 font-main space-y-5">
-            <motion.div
-                initial={{opacity: 0, y: -20}}
-                animate={{opacity: 1, y: 0}}
-                transition={{duration: 0.5, ease: "backOut", delay: 0.75}}
-                className={"flex mb-10 sm:mb-20 justify-center"}
-            >
-                <Image
-                    fetchPriority={"high"}
-                    priority={true}
-                    aria-label={"Dysnomia logo"}
-                    className={"drop-shadow-accent max-sm:w-1/2"}
-                    src={"./icon.svg"}
-                    alt={"logo"}
-                    width={300}
-                    height={300} />
-            </motion.div>
-            <SplitText
-                ref={scope}
-                split={"symbol"}
-                initial={{opacity: 0}}
-                childrenClassName={"title"}
-                className={"md:text-5xl text-4xl"}>
-                Dysnomia
-            </SplitText>
-            <SplitText initial={{y: 10, opacity: 0}} ref={scope} childrenClassName={"slogan"}
-                       className="md:text-2xl text-xl text-center">Free open source messenger that values your
-                <span className={"text-accent font-bold text-shadow-glow"}> privacy</span>.</SplitText>
-            <div
-                ref={scope}
-                className="flex justify-evenly sm:space-x-10 max-sm:flex-col max-sm:space-y-4"
-                >
+        <div className="w-full h-screen flex flex-col justify-center">
+            <div className="flex flex-col items-center justify-center p-6 font-main space-y-5">
                 <motion.div
-                    className={"buttons h-fit w-fit place-self-center"}
-                    initial={{opacity: 0, y: 50}}
+                    initial={{opacity: 0, y: -20}}
+                    animate={{opacity: 1, y: 0}}
+                    transition={{duration: 0.5, ease: "backOut", delay: 0.75}}
+                    className={"flex mb-10 sm:mb-20 justify-center"}
                 >
-                    <Link className={"h-fit place-self-center"} href={"/login"}><Button onClickAction={() => {}}>Get Started</Button></Link>
+                    <Image
+                        fetchPriority={"high"}
+                        priority={true}
+                        aria-label={"Dysnomia logo"}
+                        className={"drop-shadow-accent max-sm:w-1/2"}
+                        src={"./icon.svg"}
+                        alt={"logo"}
+                        width={200}
+                        height={200} />
                 </motion.div>
-                <motion.div
-                    className={"buttons h-fit w-fit place-self-center"}
-                    initial={{opacity: 0, y: 50}}
+                <SplitText
+                    ref={scope}
+                    split={"symbol"}
+                    initial={{opacity: 0}}
+                    childrenClassName={"title"}
+                    className={"md:text-5xl text-4xl"}>
+                    Dysnomia
+                </SplitText>
+                <SplitText initial={{y: 10, opacity: 0}} ref={scope} childrenClassName={"slogan"}
+                           className="md:text-2xl text-xl text-center">Free open source messenger that values your
+                    <span className={"text-accent font-bold text-shadow-glow"}> privacy</span>.</SplitText>
+                <div
+                    ref={scope}
+                    className="flex justify-evenly sm:space-x-10 max-sm:flex-col max-sm:space-y-4"
                 >
-                    <Button variant={"outline"} onClickAction={() => {}}>About Us</Button>
-                </motion.div>
+                    <motion.div
+                        className={"buttons h-fit w-fit place-self-center"}
+                        initial={{opacity: 0, y: 50}}
+                    >
+                        <Link className={"h-fit place-self-center"} href={"/login"}><Button onClickAction={() => {}}>Get Started</Button></Link>
+                    </motion.div>
+                    <motion.div
+                        className={"buttons h-fit w-fit place-self-center"}
+                        initial={{opacity: 0, y: 50}}
+                    >
+                        <Button variant={"outline"} onClickAction={() => {}}>About Us</Button>
+                    </motion.div>
+                </div>
             </div>
         </div>
     );
