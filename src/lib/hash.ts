f0f0079629d48a490a88f0bc004f77a19a396186
lZh1r <electroxhackerpig09@gmail.com>
2025-10-03 21:59:07 +0300

    removed useless hash implementation

--- src/lib/hash.ts
+++
@@ -1,5 +0,0 @@
-export default async function hash(algorithm: string, input: string) {
-    const arrayBuffer = await crypto.subtle.digest(algorithm, new TextEncoder().encode(input));
-    const array = Array.from(new Uint8Array(arrayBuffer));
-    return array.map(b => b.toString(16).padStart(2, '0')).join('');
-}
