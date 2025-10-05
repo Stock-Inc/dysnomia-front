7a1ab4444a0331cc30e3394d314b5b35ab8b79b6
lZh1r <electroxhackerpig09@gmail.com>
2025-10-05 20:25:23 +0300

    removed a todo

--- src/components/home/chat/input/CommandList.tsx
+++ src/components/home/chat/input/CommandList.tsx
@@ -45,7 +45,6 @@ export default function CommandList(
                     );
                 }
             }
-            //TODO: set input on click
         );
         return result;
     }, [commands, input, setInput]);
