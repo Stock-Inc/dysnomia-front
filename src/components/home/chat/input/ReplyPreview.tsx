3fb769de4d575b9b1153b2fb4e5da2b9081a9d39
lZh1r <electroxhackerpig09@gmail.com>
2025-10-05 21:46:28 +0300

    added line clamp to reply preview text

--- src/components/home/chat/input/ReplyPreview.tsx
+++ src/components/home/chat/input/ReplyPreview.tsx
@@ -18,7 +18,7 @@ export default function ReplyPreview(
                             border-card-border group-has-focus:border-accent bg-light-background flex justify-between transition-all`}>
             <div className={"flex space-x-2 p-2"}>
                 <p className={"text-lg"}>{messageToReplyTo?.name === "" ? "anon" : messageToReplyTo?.name}:</p>
-                <q className={"text-sm place-self-center"}>{messageToReplyTo?.message}</q>
+                <q className={"text-sm place-self-center line-clamp-2"}>{messageToReplyTo?.message}</q>
             </div>
             <button aria-label={"Cancel reply"} onClick={cancelReplyAction}
                     className={`place-self-center cursor-pointer transition-all focus:outline-none hover:text-accent
