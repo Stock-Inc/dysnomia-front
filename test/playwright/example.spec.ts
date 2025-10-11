c25901143cdd351b7a84625c879a88aefca1cbe4
lZh1r <electroxhackerpig09@gmail.com>
2025-10-11 16:17:18 +0300

    DYS-39 login testing

--- test/playwright/example.spec.ts
+++ test/playwright/example.spec.ts
@@ -1,11 +1,22 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("homepage", () => {
-    test("navigating to login screen", async ({page}) => {
+test.describe("unauthorized", () => {
+    test.use({storageState: ""});
+    test("should navigate to login screen", async ({page}) => {
         await page.goto("http://localhost:3000");
         await expect(page.getByText("Get Started")).toBeVisible();
         await page.getByText("Get Started").click();
         await page.waitForURL("http://localhost:3000/login", {timeout: 10000});
         await expect(page.getByText("Login")).toBeVisible();
+    });
+    test("should be able to log in", async ({page}) => {
+        await page.goto("http://localhost:3000/login");
+        await expect(page.getByText("Login")).toBeVisible();
+        await page.getByPlaceholder("Username").fill(process.env.PLAYWRIGHT_LOGIN!);
+        await page.getByPlaceholder("Password").fill(process.env.PLAYWRIGHT_PASSWORD!);
+        await page.getByText("Login").click();
+        await page.waitForURL("http://localhost:3000/home", {timeout: 10000});
+        await page.context().storageState({path: "auth.json"});
+        await expect(page.getByText("Chats")).toBeVisible();
     });
 });
