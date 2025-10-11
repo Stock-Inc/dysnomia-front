2f9af4e5b60a25a189ca1e253d05fbdfcefd0916
lZh1r <electroxhackerpig09@gmail.com>
2025-10-11 16:17:52 +0300

    DYS-39 testing chat loading and message sending

--- test/playwright/example.spec.ts
+++ test/playwright/example.spec.ts
@@ -20,3 +20,21 @@ test.describe("unauthorized", () => {
         await expect(page.getByText("Chats")).toBeVisible();
     });
 });
+
+test.describe("chat", () => {
+    test.use({storageState: "auth.json"});
+    test.beforeEach(async ({page}) => {
+        await page.goto("http://localhost:3000/home");
+        await expect(page.getByText("Chats")).toBeVisible();
+        await page.getByText("public").click();
+        await page.getByLabel("Toggle sidebar").click();
+        await expect(page.getByText("Chats")).toHaveCSS("opacity", "0");
+    });
+    const message = crypto.randomUUID().toString();
+    test("should be able to load public chat and send a message", async ({page}) => {
+        await expect(page.getByPlaceholder("Write a message...")).toBeVisible();
+        await page.getByPlaceholder("Write a message...").fill(message);
+        await page.getByLabel("Send button").click();
+        await expect(page.getByText(message).filter({visible: true})).toBeVisible();
+    });
+});
