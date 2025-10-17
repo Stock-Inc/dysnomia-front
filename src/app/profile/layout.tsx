37cda39f4a931b4850e46984c57d9cabb9d7956f
lZh1r <electroxhackerpig09@gmail.com>
2025-10-17 21:13:45 +0300

    DYS-65 disabled ssr for the whole /profile route (why am I even using next)

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,13 +1,21 @@
+"use client";
 import BackButton from "@/components/profile/BackButton";
+import NoSSR from "@/components/NoSSR";
 
 export default function HomeLayout({children}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <div className="">
-            <BackButton/>
-            {children}
-        </div>
+        <NoSSR>
+            <div>
+                <BackButton/>
+                <div className={"h-screen flex flex-col justify-center items-center font-main"}>
+                    <div className={"p-8 bg-light-background border-2 border-card-border rounded-2xl flex flex-col space-y-4"}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </NoSSR>
     );
 
 }
