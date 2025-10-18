36d34cc0dc2c124da6b201882aec1fa9cb3701aa
lZh1r <electroxhackerpig09@gmail.com>
2025-10-18 22:23:09 +0300

    DYS-71 basic preferences settings component

---
+++ src/components/settings/PreferencesSettings.tsx
@@ -0,0 +1,34 @@
+import {persistentStore, Theme} from "@/lib/app-store";
+import {useState} from "react";
+
+export default function PreferencesSettings() {
+    const store = persistentStore();
+    const [chosenTheme, setChosenTheme] = useState<Theme>(store.theme);
+    return (
+        //TODO: next-themes
+        <div>
+            <form className={"text-xl flex flex-col p-4"}>
+                <div className={"flex space-x-3"}>
+                    <label htmlFor={"theme"}>Theme</label>
+                    <select onChange={(e) => {
+                        setChosenTheme(e.currentTarget.value as Theme);
+                    }} value={chosenTheme} id={"theme"} className={"focus:outline-none bg-card-border rounded-xl p-1"}>
+                        <option value={"dark"}>Dark</option>
+                        <option value={"light"}>Light</option>
+                    </select>
+                </div>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        store.setTheme(chosenTheme);
+                    }}
+                    className={`hover:bg-card-border hover:text-accent border-2 border-card-border 
+                    rounded-2xl mt-5 p-2 transition-all cursor-pointer focus:outline-none`}
+                    type={"submit"}
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
