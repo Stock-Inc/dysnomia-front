c83d92c0b7adf986834dc4b570c60ccd5ac3eee1
lZh1r <electroxhackerpig09@gmail.com>
2025-10-18 15:14:27 +0300

    reverted profile routing stuff

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,19 @@
 "use client";
-import LocalProfileDetails from "@/components/profile/LocalProfileDetails";
-import {QueryClient} from "@tanstack/query-core";
-import {QueryClientProvider} from "@tanstack/react-query";
+
+import {persistentStore} from "@/lib/app-store";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 export default function Page() {
 
-    const queryClient = new QueryClient();
+    const store = persistentStore();
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push(`/profile/${store.username}`);
+    }, []);
 
     return (
-        <div>
-            <h1 className={"text-center text-3xl mb-4"}>My Profile</h1>
-            <QueryClientProvider client={queryClient}>
-                <LocalProfileDetails/>
-            </QueryClientProvider>
-        </div>
+        <></>
     );
 }
