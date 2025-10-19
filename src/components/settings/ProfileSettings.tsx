57f79555ad4c0c5dcbc2bc21cd4915bc52322a0a
lZh1r <electroxhackerpig09@gmail.com>
2025-10-19 12:43:19 +0300

    DYS-71 zod validation and api calls in profile settings

--- src/components/settings/ProfileSettings.tsx
+++ src/components/settings/ProfileSettings.tsx
@@ -1,11 +1,19 @@
 "use client";
-//TODO: merge with ProfileDetails
 import {persistentStore} from "@/lib/app-store";
 import {Camera, User} from "lucide-react";
-import {useQuery} from "@tanstack/react-query";
+import {useMutation, useQuery} from "@tanstack/react-query";
 import useCookie from "@/hook/useCookie";
-import {useState} from "react";
 import {ProfileDetails} from "@/components/profile/ProfileDetails";
+import {z} from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+    displayName: z.string()
+        .min(3, {message: "Display name has to be at least 3 characters long!"})
+        .max(20, {error: "Display name can not be longer than 20 symbols"}),
+    profileDescription: z.string()
+});
 
 export default function ProfileSettings() {
     const store = persistentStore();
@@ -22,13 +30,38 @@ export default function ProfileSettings() {
         ).then(res => res.json() as unknown as ProfileDetails),
         enabled: !!token && !!store.username
     });
-    const [newName, setNewName] = useState("");
-    const [newBio, setNewBio] = useState("");
+    const {mutate} = useMutation({
+        mutationFn: (body: z.infer<typeof formSchema>) => fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/profile/edit_info`,
+            {
+                method: "PATCH",
+                body: JSON.stringify(body),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }
+            }
+        )
+    });
+
+    const form = useForm({
+        resolver: zodResolver(formSchema)
+    });
+
+    async function handleSubmit(values: z.infer<typeof formSchema>) {
+        form.clearErrors();
+        mutate(values, {
+            onSuccess: (_) => {
+                store.setDisplayName(values.displayName);
+                store.setProfileDescription(values.profileDescription);
+            },
+            onError: (err) => console.log(err),
+        });
+    }
 
     return (
-        //TODO: zod + react hook form
         <div className={"flex justify-evenly items-start flex-col max-h-[80vh] p-4 text-xl"}>
-            <form className={"space-y-4 flex flex-col w-full"}>
+            <form className={"space-y-4 flex flex-col w-full"} onSubmit={form.handleSubmit(handleSubmit)}>
                 <button
                     onClick={(e) => e.preventDefault()}
                     className={`w-fit h-fit flex justify-center group rounded-2xl place-self-center 
@@ -38,15 +71,19 @@ export default function ProfileSettings() {
                     <Camera className={`absolute w-20 h-20 place-self-center p-2 rounded-full transition-all group-has-hover:text-accent`}/>
                 </button>
                 <div className={"w-full"}>
-                    <label htmlFor={"displayName"} className={"text-lg text-muted-foreground"}>Display Name</label>
+                    <label
+                        htmlFor={"displayName"}
+                        className={`${!!form.formState.errors.displayName ? "text-error" : "text-muted-foreground"} text-lg transition-all`}>
+                        Display Name
+                    </label>
                     <input
-                        //TODO: zod validation
-                        onChange={(e) => setNewName(e.currentTarget.value)}
+                        {...form.register("displayName")}
                         autoComplete={"off"}
                         id={"displayName"}
                         maxLength={64}
                         className={
-                            `focus:outline-none border-foreground transition-all border-2 focus:border-accent w-full rounded-2xl p-1`
+                            `${!!form.formState.errors.displayName ? "border-error" : "border-foreground"}
+                            focus:outline-none transition-all border-2 focus:border-accent w-full rounded-2xl p-1`
                         }
                         defaultValue={store.displayName || data?.displayName}
                     />
@@ -55,7 +92,7 @@ export default function ProfileSettings() {
                     <div className={`${!(isLoading || !!error) && "flex flex-col space-x-2"}`}>
                         <label className={"text-lg text-muted-foreground"} htmlFor={"bio"}>Bio</label>
                         <input
-                            onChange={(e) => setNewBio(e.currentTarget.value)}
+                            {...form.register("profileDescription")}
                             id={"bio"}
                             autoComplete={"off"}
                             maxLength={512}
@@ -67,16 +104,10 @@ export default function ProfileSettings() {
                         />
                     </div>
                 </div>
+                <p className={"text-error text-lg"}>{form.formState.errors.displayName?.message}</p>
                 <button
-                    onClick={(e) => {
-                        e.preventDefault();
-                        if (newName.trim().length >= 3) {
-                            store.setDisplayName(newName.trim());
-                        }
-                        store.setProfileDescription(newBio.trim());
-                    }}
-                    className={`hover:bg-card-border border-2 hover:text-accent border-card-border 
-                    rounded-2xl mt-5 p-2 transition-all cursor-pointer focus:outline-none`}
+                    className={`enabled:hover:bg-card-border border-2 enabled:hover:text-accent border-card-border 
+                    rounded-2xl p-2 transition-all enabled:cursor-pointer enabled:focus:outline-none disabled:text-muted-foreground`}
                     type={"submit"}
                 >
                     Apply
