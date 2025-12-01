137edb9c31e2f55927460ab91ca25982d4b20003
lZh1r <electroxhackerpig09@gmail.com>
2025-12-01 08:51:51 +0300

    DYS-76 sidebar chats fetching

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,14 +2,19 @@
 import ProfileModal from "@/components/home/ProfileModal";
 import NoSSR from "@/components/NoSSR";
 import Sidebar from "@/components/home/Sidebar";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {QueryClient} from "@tanstack/query-core";
 
 export default function HomeLayout({children}: Readonly<{
     children: React.ReactNode;
 }>) {
+    const client = new QueryClient();
     return (
         <NoSSR>
             <div className="flex">
-                <Sidebar/>
+                <QueryClientProvider client={client}>
+                    <Sidebar/>
+                </QueryClientProvider>
                 <ProfileModal/>
                 {children}
             </div>
